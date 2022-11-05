060fe8cc9a05b8bc29e498555260090f62bb9b30
TutoDevWeb <TutoDevWeb@outlook.fr>
2022-11-05 14:26:00 +0100

    Je modifie le contrôleur pour aller chercher les données.

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
 {
     public function faq()
     {
-        return view('faq');
+        return view('faq', Item::all());
     }
 }
